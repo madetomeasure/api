3b8b37ca5755e574e07f4ebbb38b74e3223d1a35
Matt Kirk <matt@matthewkirk.com>
2015-03-03 09:26:55 -0800

    Dependencies and new procfile

---
+++ api.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+        "fmt"
+        "net/http"
+        "github.com/gorilla/mux"
+        "os"
+) 
+
+func main() {
+  r := mux.NewRouter();
+  r.HandleFunc("/", HomeHandler)
+  welcome()
+  http.ListenAndServe(":" + port(), r)
+}
+
+func port() string {
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+  return port
+}
+
+func welcome() {
+  fmt.Println("=> Booting...")
+  fmt.Println("=> Made to Measure Alpha application starting in development on http://0.0.0.0:" + port()) 
+  fmt.Println("=> Ctrl-C to shutdown Server")
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Add("Content-Type", "text/html")
+  fmt.Fprintf(w, "Hello World!")
+}
+
