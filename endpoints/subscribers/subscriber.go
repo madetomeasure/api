5f06d1176e4b806a3d09b3d925c985986c914c41
Matt Kirk <matt@matthewkirk.com>
2015-03-25 17:42:45 -0700

    Adding rendering ability

--- endpoints/subscribers/subscriber.go
+++ endpoints/subscribers/subscriber.go
@@ -1,8 +1,21 @@
 package subscriber
-import("net/http")
+
+import(
+  "github.com/madetomeasure/api/lib/render"
+  "github.com/jinzhu/gorm"
+  "net/http"
+)
+
+type Subscriber struct {
+  Id int64
+  CreatedAt time.Time
+  UpdatedAt time.Time
+  Name      string
+}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(http.StatusNotImplemented)
+  subscribers := db.First(&subscriber, 10)
+  render.Perform(&subscribers)
 }
 
 func CreateHandler(w http.ResponseWriter, r *http.Request) {
